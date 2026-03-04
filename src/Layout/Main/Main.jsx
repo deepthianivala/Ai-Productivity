258e3b5eab8c033a87f440e06e73184f80aaff81
Deepthi Anivala <deepthianivala@gmail.com>
2026-03-04 16:03:07 +0530

    added new note creation feature

--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Display from "../Display/Display";
 import Noteholder from "../../Component/Noteholder";
 
 const Main = () => {
+  const [notedata, setnotesdata] = useState([
+    {
+      id: 1,
+      title: "Project Apollo: Q3 Roadmap",
+      summary:
+        "Focus on scaling the infrastructure to support 1M+ users. Priorities include Database Sharding, Edge Functions implementation, and a complete UI overhaul for the mobile app.",
+      tags: ["WORK", "STRATEGY"],
+      aiSummarized: true,
+    },
+    {
+      id: 2,
+      title: "Customer Retention Plan",
+      summary:
+        "Analyze churn patterns and introduce loyalty programs. Plan includes personalized notifications and targeted discounts.",
+      tags: ["MARKETING", "ANALYTICS"],
+      aiSummarized: true,
+    },
+    {
+      id: 3,
+      title: "AI Productivity Tool",
+      summary:
+        "Develop an internal AI tool that summarizes long documents and extracts key action points for teams.",
+      tags: ["AI", "PRODUCTIVITY"],
+      aiSummarized: true,
+    },
+    {
+      id: 3,
+      title: "AI Productivity Tool",
+      summary:
+        "Develop an internal AI tool that summarizes long documents and extracts key action points for teams.",
+      tags: ["AI", "PRODUCTIVITY"],
+      aiSummarized: true,
+    },
+  ]);
   return (
     <div className="border border-red-900 ml-60 w-full bg-[#fcfaf7] min-h-screen">
-      <Navbar />
+      <Navbar notesdummydata={notedata} setnotesdata={setnotesdata} />
       <div className="p-8 flex flex-col">
         <Display />
-        <Noteholder />
+        <Noteholder notesdummydata={notedata} />
       </div>
     </div>
   );
--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -2,14 +2,39 @@ import React from "react";
 import Searchlogo from "../../assets/searchlogo.png";
 import Notificationslogo from "../../assets/notifications.png";
 
-const Navbar = () => {
+const Navbar = ({ notesdummydata, setnotesdata }) => {
+  //  {
+  //     id: 1,
+  //     title: "Project Apollo: Q3 Roadmap",
+  //     summary:
+  //       "Focus on scaling the infrastructure to support 1M+ users. Priorities include Database Sharding, Edge Functions implementation, and a complete UI overhaul for the mobile app.",
+  //     tags: ["WORK", "STRATEGY"],
+  //     aiSummarized: true,
+  //   },
+  const handlenewnote = () => {
+    // onclick handler for the "New Note" button. When the button is clicked, this function will be executed.
+    console.log("new note added");
+    const freshnote = {
+      id: notesdummydata.length + 1,
+      title: "New Note",
+      summary: "This is a new note. Click to edit.",
+      tags: ["#Mynew note"],
+      aiSummarized: false,
+    };
+
+    setnotesdata([...notesdummydata, freshnote]); //spread operator is used to create a new array that includes all the existing notes from notesdummydata, followed by the new note (freshnote). This way, we are not mutating the original array but creating a new one with the added note.
+  };
+
   return (
     <div className=" p-4 flex justify-around ">
       <div className="border border-[#d1ded0] w-[75%] p-2 rounded-xl flex">
         <img src={Searchlogo} />
         <input className="ml-5 w-full outline-none" placeholder="Search..." />
       </div>
-      <div className="bg-[#e07a5f] w-[12%] rounded-xl text-white font-inter flex justify-center items-center">
+      <div
+        onClick={handlenewnote}
+        className="bg-[#e07a5f] w-[12%] rounded-xl text-white font-inter flex justify-center items-center"
+      >
         + New Note
       </div>
 
